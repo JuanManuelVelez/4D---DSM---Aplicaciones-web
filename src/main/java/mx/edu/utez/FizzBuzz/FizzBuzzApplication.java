c31896f69d6523bf673824669c5117d3fabc8260
Calvo Velez Juan Manuel <20253ds110@utez.edu.mx>
2026-09-24 22:02:35 -0600

    Servicios FizzBuzz y Fibonacci

---
+++ src/main/java/mx/edu/utez/FizzBuzz/FizzBuzzApplication.java
@@ -0,0 +1,45 @@
+package mx.edu.utez.FizzBuzz.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ServiciosController {
+
+	@GetMapping("/fizzbuzz/servicio1/{n}")
+	public String fizzBuzz(@PathVariable int n) {
+		for (int i = 1; i <= n; i++) {
+			if (i % 3 == 0 && i % 5 == 0) {
+				System.out.println("FizzBuzz");
+			} else if (i % 3 == 0) {
+				System.out.println("Fizz");
+			} else if (i % 5 == 0) {
+				System.out.println("Buzz");
+			} else {
+				System.out.println(i);
+			}
+		}
+		return "Juan Manuel Calvo Velez";
+	}
+
+	@GetMapping("/fibonacci/servicio2/{n}")
+	public String fibonacci(@PathVariable int n) {
+		long anterior = 0;
+		long actual = 1;
+
+		for (int i = 0; i < n; i++) {
+			if (i == 0) {
+				System.out.println(anterior);
+			} else if (i == 1) {
+				System.out.println(actual);
+			} else {
+				long siguiente = anterior + actual;
+				System.out.println(siguiente);
+				anterior = actual;
+				actual = siguiente;
+			}
+		}
+		return "Juan Manuel Calvo Velez";
+	}
+}
